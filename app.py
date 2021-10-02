3d5fe206d3832fcea62b4864f6d1b328a82f4d1f
TheLegend108 <jw.barker@outlook.com>
2021-10-02 13:11:10 +0100

    added caching and retrieval of stock price data

--- app.py
+++ app.py
@@ -35,25 +35,56 @@ class Stock:
     gained: bool = False
 
     def __post_init__(self):
-        if not self.data:
+        if self.data is None:
+            # no data for this stock was present in cache, so fetch new data
+
+            print("getting values")
             self.data = get_values(
                 parse_date(self.date_bought),
                 parse_date(self.date_sold),
                 self.ticker,
                 exchange=self.exchange,
             )
-        # apply currency conversion (if required):
-        if self.currency != "GBP":
-            self.data["value"] = self.data.apply(
-                lambda row: convert_currency(row["value"], row["time"], self.currency)
-                * 100,
-                axis=1,
-            )
+            # apply currency conversion (if required):
+            if self.currency != "GBP":
+                self.data["value"] = self.data.apply(
+                    lambda row: convert_currency(
+                        row["value"], row["time"], self.currency
+                    )
+                    * 100,
+                    axis=1,
+                )
 
-        # apply commission/fx charge using book price
-        self.data.loc[(self.data.index[0], "value")] = (
-            self.book_cost * 100 / self.holding
-        )  # equivalent to self.data.iloc[0]["value"], but prevents SettingWithCopyWarning
+            # apply commission/fx charge using book price
+            self.data.loc[(self.data.index[0], "value")] = (
+                self.book_cost * 100 / self.holding
+            )  # equivalent to self.data.iloc[0]["value"], but prevents SettingWithCopyWarning
+            print(self.data)
+
+        else:
+            # data was cached, but is not fully up to date
+            last_date = self.data.index[-1].date()
+            # to avoid confusion/out of date data, remove all data generated on this date
+            self.data = self.data[self.data["time"].dt.date != last_date]
+
+            new_data = get_values(
+                last_date,
+                parse_date(self.date_sold),
+                self.ticker,
+                exchange=self.exchange,
+            )
+            new_data.drop_duplicates(inplace=True)
+            if self.currency != "GBP":
+                new_data["value"] = new_data.apply(
+                    lambda row: convert_currency(
+                        row["value"], row["time"], self.currency
+                    )
+                    * 100,
+                    axis=1,
+                )
+
+            self.data = pd.concat([self.data, new_data])
+
 
 
 def load_portfolio(file="portfolio.json") -> List[Stock]:
@@ -61,24 +92,38 @@ def load_portfolio(file="portfolio.json") -> List[Stock]:
     plus a dataframe showing prices between start date and end date"""
     with open(file, "r") as f:
         stock_list = json.load(f)
+
+    # load in cached data
+    imported_data = pd.read_csv("stock_cache.csv")
+    imported_data["time"] = pd.to_datetime(imported_data["time"])
+
     rep = []
     # create stock objects
 
     for stock in stock_list:
+        name = stock["name"]
+
+        # check if stock has any data cached, and if it does, assign it to the new stock
+        if name in list(imported_data.columns):
+            stock["data"] = imported_data[["time", name]].dropna()
+            stock["data"].columns = ["time", "value"]
+
+            stock["data"].index = stock["data"]["time"]
+
         new_stock = Stock(**stock)
         rep.append(new_stock)
 
     # once all stocks have been created and __post_init__() has run, save to cache
 
-    #last row in each df is dropped as this is a real-time value and may not be applicable in future
+    # last row in each df is dropped as this is a real-time value and may not be applicable in future
     to_cache = pd.concat(
         [stock.data.drop("time", axis=1).drop(stock.data.index[-1]) for stock in rep],
         axis=1,
         ignore_index=True,
     )
     to_cache.columns = [stock.name for stock in rep]
-    print(to_cache)
     to_cache.to_csv("stock_cache.csv", index_label="time")
+
     return rep
 
 
@@ -105,7 +150,9 @@ def get_values(
             raw.index.to_series() + dt.timedelta(hours=times["close"]),
         ]
     )
-    rep = rep.rename(index={rep.index[-1]: dt.datetime.now()})
+    rep: pd.DataFrame = rep.rename(index={rep.index[-1]: dt.datetime.now()})
+    if rep.index[-1]<rep.index[-2]:
+        rep.drop(rep.index[-1], inplace=True)
     return rep.sort_index().assign(time=rep.index.values)
 
 
@@ -158,81 +205,90 @@ def parse_date(date_string) -> dt.datetime:
 PORTFOLIO = load_portfolio()
 TOTAL_VALUE = merge_portfolio(PORTFOLIO)
 
-dropdown_options = [{"label": stock.name, "value": stock.ticker} for stock in PORTFOLIO]
-dropdown_options[0:0] = [
-    {"label": "Portfolio Percentage Loss/Gain", "value": "PERCENT.LG"},
-    {"label": "Portfolio Actual Loss/Gain", "value": "ACTUAL.LG"},
-]
-app = dash.Dash(__name__)
-app.layout = html.Div(
-    children=[
-        dcc.Dropdown(
-            id="ticker_dropdown",
-            options=dropdown_options,
-            value="PERCENT.LG",
-            clearable=False,
-        ),
-        dcc.Graph(id="time-series-chart"),
+
+def main():
+    dropdown_options = [
+        {"label": stock.name, "value": stock.ticker} for stock in PORTFOLIO
     ]
-)
-
-
-@app.callback(
-    Output("time-series-chart", "figure"), [Input("ticker_dropdown", "value")]
-)
-def update_graph(ticker):
-    data = TOTAL_VALUE
-    COLOUR_GREEN = "#00ff04"
-    COLOUR_RED = "red"
-    color = COLOUR_GREEN
-    if ticker == "PERCENT.LG":
-        if data.iloc[-1]["percent_change"] < 0:
-            color = COLOUR_RED
-        fig = px.line(data, x=data.index, y="percent_change")
-        fig.update_traces(line_color=color)
-        fig.update_layout(yaxis_title="Percent change in portfolio value")
-
-    elif ticker == "ACTUAL.LG":
-        if data.iloc[-1]["actual_change"] < data.iloc[0]["actual_change"]:
-            color = COLOUR_RED
-        fig = px.line(data, x=data.index, y=data["actual_change"] / 100)
-        fig.update_traces(line_color=color)
-        fig.update_layout(yaxis_title="Change in portfolio value (pounds)")
-
-    else:
-        for stock in PORTFOLIO:
-            if stock.ticker == ticker:
-                data = stock.data["value"]
-                break
-        color = "#00ff04"
-        if data.iloc[-1] < data.iloc[0]:
-            color = "red"
-        fig = px.line(data, x=data.index, y="value")
-        fig.update_traces(line_color=color)
-        fig.update_layout(yaxis_title=f"Value of {ticker} (pence)")
-
-    fig.update_layout(xaxis_title="Date")
-    fig.update_layout(
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list(
-                    [
-                        dict(count=1, label="1m", step="month", stepmode="backward"),
-                        dict(count=6, label="6m", step="month", stepmode="backward"),
-                        dict(count=1, label="YTD", step="year", stepmode="todate"),
-                        dict(count=1, label="1y", step="year", stepmode="backward"),
-                        dict(step="all"),
-                    ]
-                )
+    dropdown_options[0:0] = [
+        {"label": "Portfolio Percentage Loss/Gain", "value": "PERCENT.LG"},
+        {"label": "Portfolio Actual Loss/Gain", "value": "ACTUAL.LG"},
+    ]
+    app = dash.Dash(__name__)
+    app.layout = html.Div(
+        children=[
+            dcc.Dropdown(
+                id="ticker_dropdown",
+                options=dropdown_options,
+                value="PERCENT.LG",
+                clearable=False,
             ),
-            rangeslider=dict(visible=True),
-            type="date",
-        )
+            dcc.Graph(id="time-series-chart"),
+        ]
     )
-    return fig
+
+    @app.callback(
+        Output("time-series-chart", "figure"), [Input("ticker_dropdown", "value")]
+    )
+    def update_graph(ticker):
+        data = TOTAL_VALUE
+        COLOUR_GREEN = "#00ff04"
+        COLOUR_RED = "red"
+        color = COLOUR_GREEN
+        if ticker == "PERCENT.LG":
+            if data.iloc[-1]["percent_change"] < 0:
+                color = COLOUR_RED
+            fig = px.line(data, x=data.index, y="percent_change")
+            fig.update_traces(line_color=color)
+            fig.update_layout(yaxis_title="Percent change in portfolio value")
+
+        elif ticker == "ACTUAL.LG":
+            if data.iloc[-1]["actual_change"] < data.iloc[0]["actual_change"]:
+                color = COLOUR_RED
+            fig = px.line(data, x=data.index, y=data["actual_change"] / 100)
+            fig.update_traces(line_color=color)
+            fig.update_layout(yaxis_title="Change in portfolio value (pounds)")
+
+        else:
+            for stock in PORTFOLIO:
+                if stock.ticker == ticker:
+                    data = stock.data["value"]
+                    break
+            color = "#00ff04"
+            if data.iloc[-1] < data.iloc[0]:
+                color = "red"
+            fig = px.line(data, x=data.index, y="value")
+            fig.update_traces(line_color=color)
+            fig.update_layout(yaxis_title=f"Value of {ticker} (pence)")
+
+        fig.update_layout(xaxis_title="Date")
+        fig.update_layout(
+            xaxis=dict(
+                rangeselector=dict(
+                    buttons=list(
+                        [
+                            dict(
+                                count=1, label="1m", step="month", stepmode="backward"
+                            ),
+                            dict(
+                                count=6, label="6m", step="month", stepmode="backward"
+                            ),
+                            dict(count=1, label="YTD", step="year", stepmode="todate"),
+                            dict(count=1, label="1y", step="year", stepmode="backward"),
+                            dict(step="all"),
+                        ]
+                    )
+                ),
+                rangeslider=dict(visible=True),
+                type="date",
+            )
+        )
+        return fig
+
+    """Popen(
+        [r"C:\Program Files\Google\Chrome\Application\chrome.exe", "http://127.0.0.1:8050"]
+    )"""
+    app.run_server(debug=True)
 
 
-"""Popen(
-    [r"C:\Program Files\Google\Chrome\Application\chrome.exe", "http://127.0.0.1:8050"]
-)"""
-app.run_server(debug=True)
+main()
